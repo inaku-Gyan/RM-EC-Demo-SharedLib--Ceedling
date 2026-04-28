42e23a04695180ae2b54886993a9d92d015b88de
Gyan <inaku.gs@gmail.com>
2026-04-28 22:56:59 +0800

    fix HAL 库链接错误

--- src/source/hal_ext/tests/test_tim.c
+++ src/source/hal_ext/tests/test_tim.c
@@ -6,6 +6,10 @@
     #include "../tim.h"
 TEST_SOURCE_FILE("../tim.c")
 
+// tim.c 调用 HAL_RCC_*；mixin 不挂 vendor .c（cortex/cmsis_gcc.h 等含 ARM
+// 内联汇编无法在 host 编译），故走 deps/stubs/hal_f4 提供的 host 端桩。
+TEST_SOURCE_FILE("sl_hal_f4_stubs.c")
+
 void setUp(void) {
 }
 void tearDown(void) {
